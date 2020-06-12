2c40487aa9cf44f4e46de4546a2e63188b583932
leorvieira <leonardorangelvieira@gmail.com>
2020-06-12 07:53:40 -0300

    first commit

--- js/app/services/FornecedorService.js
+++ js/app/services/FornecedorService.js
@@ -1,9 +1,64 @@
 
-class CorridaService {
+class FornecedorService {
 
     constructor() {
         this._http = new HttpService();
     }
+
+    consultaFornecedor(cnpj) {
+        console.log("dentro do consulta cliente = "+cnpj);
+        return new Promise((resolve, reject) => {
+            this._http.getF('https://rws-onparty-middleware.herokuapp.com/v1/suppliers/cnpj/'+cnpj).then
+                (objeto => {
+                    resolve(new Fornecedor(objeto._id, objeto._cnpj, objeto._name, objeto._email, objeto._address, objeto._number, objeto._addressComplement, objeto._telephone, objeto._cep, objeto._status))
+                    console.log("retornando da consulta fornecedor");
+                    console.log(objeto._name);
+                }).catch(
+                    erro => {
+                        console.error(erro)
+                        reject(erro);
+                    });
+        });
+    }
+
+
+    incluirFornecedor(cnpj, name, email, address, number, addressComplement, telephone, cep) {
+
+        console.log("dentro do incluir fornecedor no FornecedorService");
+
+        let url = "https://rws-onparty-middleware.herokuapp.com/v1/suppliers"
+
+        var data = {};
+        data.address = address;
+        data.addressComplement = addressComplement;
+        data.cep = cep;
+        data.cnpj = cnpj;
+        data.email = email;
+        data.id = 0;
+        data.name = name;
+        data.number = number;
+        data.status = "ACTIVE";
+        data.telephone = telephone;
+
+        let json = JSON.stringify(data);
+        console.log("antes de chamar o post request no http service ");
+        console.log(json);
+
+        return new Promise((resolve, reject) => {
+
+    
+            console.log(url);
+            this._http.postF(url, json).then
+                (objeto => {
+                    resolve(new Fornecedor(objeto._id, objeto._cnpj, objeto._name, objeto._email, objeto._address, objeto._number, objeto._addressComplement, objeto._telephone, objeto._cep, objeto._status))
+                }).catch(
+                    erro => {
+                        console.error(erro)
+                        reject('Nao foi possivel cadastrar cliente');
+                    });
+        });
+    }
+
 
     solicitarCorrida(origem, destino) {
 
--- js/app/services/HttpService.js
+++ js/app/services/HttpService.js
@@ -8,9 +8,17 @@ class HttpService {
                 if (xhr.readyState == 4) {
                     if (xhr.status == 200) {
                         let objeto = JSON.parse(xhr.responseText);
-                        resolve(new Corrida(objeto.id, objeto.userId, objeto.vehicleId, objeto.price, objeto.departureAddress, objeto.destinationAddress, objeto.timeToUser, objeto.timeToDestination, objeto.timeLeftToReachDestination, objeto.timeLeftToReachUser, objeto.status, objeto.carBrand, objeto.carModel, objeto.carLicensePlate, objeto.localization))
+                        console.log("responseText = "+xhr.responseText);
+                        resolve(new Cliente(objeto.id, objeto.cpf, objeto.name, objeto.email, objeto.address, objeto.number, objeto.addressComplement, objeto.telephone, objeto.cep, objeto.status))
+                        console.log("dentro do httpservice "+objeto.id+" "+objeto.name);
+                        
                     } else {
-                        reject("Erro ao consultar API")
+                        if (xhr.status == 404 ) {
+                            reject("Cliente não encontrado.")       
+                        }else{
+                            reject("Erro ao consultar API.")     
+                        }
+                        
                     }
                 }
             }
@@ -18,21 +26,80 @@ class HttpService {
         });
     }
 
-    post(url) {
+    getF(url) {
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', url);
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        let objeto = JSON.parse(xhr.responseText);
+                        console.log("responseText = "+xhr.responseText);
+                        resolve(new Fornecedor(objeto.id, objeto.cnpj, objeto.name, objeto.email, objeto.address, objeto.number, objeto.addressComplement, objeto.telephone, objeto.cep, objeto.status))
+                        console.log("dentro do httpservice "+objeto.id+" "+objeto.name);
+                        
+                    } else {
+                        if (xhr.status == 404 ) {
+                            reject("Cliente não encontrado.")       
+                        }else{
+                            reject("Erro ao consultar API.")     
+                        }
+                        
+                    }
+                }
+            }
+            xhr.send()
+        });
+    }
+
+    post(url, json) {
+        console.log("dentro do http service");
+        console.log(url);
+        console.log(json);
+
         return new Promise((resolve, reject) => {
             let xhr = new XMLHttpRequest();
             xhr.open('POST', url);
-            xhr.onreadystatechange = () => {
+            xhr.setRequestHeader('Content-Type', 'application/json');
+//            xhr.onreadystatechange = () => {       
+            xhr.onload = () => {
                 if (xhr.readyState == 4) {
-                    if (xhr.status == 201) {
+                    if (xhr.status == 200) {
                         let objeto = JSON.parse(xhr.responseText);
-                        resolve(new Corrida(objeto.id, objeto.userId, objeto.vehicleId, objeto.price, objeto.departureAddress, objeto.destinationAddress, objeto.timeToUser, objeto.timeToDestination, objeto.timeLeftToReachDestination, objeto.timeLeftToReachUser, objeto.status, objeto.carBrand, objeto.carModel, objeto.carLicensePlate, objeto.localization))
+                        resolve(new Cliente(objeto.id, objeto.cpf, objeto.name, objeto.email, objeto.address, objeto.number, objeto.addressComplement, objeto.telephone, objeto.cep, objeto.status))
                     } else {
                         reject("Erro ao consultar API")
                     }
                 }
             }
-            xhr.send()
+        
+            xhr.send(json)
+        });
+    }
+
+
+    postF(url, json) {
+        console.log("dentro do http service");
+        console.log(url);
+        console.log(json);
+
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open('POST', url);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+//            xhr.onreadystatechange = () => {       
+            xhr.onload = () => {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        let objeto = JSON.parse(xhr.responseText);
+                        resolve(new Fornecedor(objeto.id, objeto.cnpj, objeto.name, objeto.email, objeto.address, objeto.number, objeto.addressComplement, objeto.telephone, objeto.cep, objeto.status))
+                    } else {
+                        reject("Erro ao consultar API")
+                    }
+                }
+            }
+        
+            xhr.send(json)
         });
     }
 
